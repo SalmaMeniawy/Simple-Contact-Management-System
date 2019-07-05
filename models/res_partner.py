2ffce5fb58d882181ee4d1afe9aa05c088be2b13
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-07-05 22:08:34 +0200

    add changes .. recreate res_partner model

---
+++ models/res_partner.py
@@ -0,0 +1,28 @@
+from odoo import models, fields, api
+
+class res_partner(models.Model):
+    _name='res.partner'
+    _inherit = 'res.partner'
+
+    @api.model
+    def create(self, vals):
+        # self.send_email()
+        res = super(res_partner, self).create(vals)
+        return res
+
+    @api.model
+    def send_email(self):
+        import smtplib
+        EMAIL = "xxxx@gmail.com"
+        PASSWORD = "XXXXXXXXXX"
+        MESSAGE = "we are greeting you for creating a new contact with us"
+        user_email = self.email
+        server = smtplib.SMTP('smtplib.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login(EMAIL, PASSWORD)
+        server.sendmail(EMAIL ,user_email,MESSAGE)
+        server.quit()
+
+
+
