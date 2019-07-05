1cb63d7a1f915d01dbb73ca92135e31e4d6a5580
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-07-05 15:18:45 +0200

    create delete method to remove record from django GUI and odoo as server

--- contact/models.py
+++ contact/models.py
@@ -10,16 +10,37 @@ class Contact (models.Model):
     phone = models.CharField(max_length=14)
     date_of_birth = models.DateField()
     email = models.EmailField()
+    external_odoo_record_num = models.IntegerField(default=0)
 
-    URL = "http://localhost:8069"
-    DB = "Contact"
-    USERNAME = "admin"
-    PASSWORD = "admin"
-    COMMON = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(URL))
-    UID = COMMON.authenticate(DB, USERNAME, PASSWORD, {})
-    def save(self):
-        models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(self.URL))
-        models.execute_kw(self.DB, self.UID, self.PASSWORD, 'res.partner', 'create', [{'name','heba'}])
+    def save(self,*args,**kwargs):
+        URL = "http://localhost:8069"
+        DB = "Contact"
+        USERNAME = "admin"
+        PASSWORD = "admin"
+        COMMON = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(URL))
+        UID = COMMON.authenticate(DB, USERNAME, PASSWORD, {})
+        models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(URL))
+        self.external_odoo_record_num = models.execute_kw(DB, UID, PASSWORD, 'res.partner', 'create'
+                                                          , [{'name':self.name ,'street':self.address
+                                                                 ,'mobile':self.mobile, 'phone':self.phone ,
+                                                              'birthdate': self.date_of_birth.strftime("%Y%m%dT") ,
+                                                              'email':self.email}])
+        return super(Contact, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        URL = "http://localhost:8069"
+        DB = "Contact"
+        USERNAME = "admin"
+        PASSWORD = "admin"
+        COMMON = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(URL))
+        UID = COMMON.authenticate(DB, USERNAME, PASSWORD, {})
+        models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(URL))
+        models.execute_kw(DB, UID, PASSWORD,'res.partner', 'unlink', [[self.external_odoo_record_num]])
+        return super(Contact, self).delete(*args, **kwargs)
+
+
+
+
 
     def __str__(self):
         return self.name
